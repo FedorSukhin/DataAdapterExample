8750283898f09b38f151b2a02e2a0c7fa812d131
User <User@DESKTOP-K15QGHT>
2023-10-17 21:05:16 +0300

    finish

--- DataAdapterExample/MainWindow.xaml.cs
+++ DataAdapterExample/MainWindow.xaml.cs
@@ -43,27 +43,28 @@ namespace DataAdapterExample
         }
         private void fillDataUser()
         {
-            dataSet = new DataSet();
-            dataAdapter1.Fill(dataSet, "computer_game");
-            dataGrid1.ItemsSource = dataSet.Tables["computer_game"].DefaultView;
+            dataSet1 = new DataSet();
+            dataAdapter1.Fill(dataSet1, "computer_game");
+            dataGrid1.ItemsSource = dataSet1.Tables["computer_game"].DefaultView;
         }
         public MainWindow()
         {
             InitializeComponent();
             // подготовка объектов
             dataAdapter = new SqlDataAdapter(fillCmd, GetDbConnection());
-            
-
+            dataAdapter1 = new SqlDataAdapter(UserCmd, GetDbConnection());
             SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);  // строка заполняет INSERT/UPDATE/DELETE-команды
             // реализацией по умолчанию
+            SqlCommandBuilder commandBuilder1 = new SqlCommandBuilder(dataAdapter1);
         }
 
         // вспомогательные поля
         private SqlDataAdapter dataAdapter;
         private SqlDataAdapter dataAdapter1;
         private DataSet dataSet;
+        private DataSet dataSet1;
         private string fillCmd = "SELECT * FROM computer_game;";
-        private string UserCmd="";
+        private string UserCmd= "SELECT * FROM computer_game;";
 
         private void fillBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -80,9 +81,15 @@ namespace DataAdapterExample
         private void runBtn_Click(object sender, RoutedEventArgs e)
         { 
             UserCmd=QweryText.Text;
-            dataAdapter1 = new SqlDataAdapter(UserCmd, GetDbConnection());
+            dataAdapter1.SelectCommand.CommandText = UserCmd;
            
             fillDataUser();
+        }
+
+        private void update1Btn_Click(object sender, RoutedEventArgs e)
+        {
+            dataAdapter1.Update(dataSet1, "computer_game");
+            fillData();
         }
     }
 }
